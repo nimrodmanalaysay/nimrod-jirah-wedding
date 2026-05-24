6db806b6267401865b2b404769cb0036a8d00af0
nimrodmanalaysay <manalaysaynimrod@gmail.com>
2026-05-24 23:55:06 +0800

    edited for vercel hosting site

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ import react from '@vitejs/plugin-react'
 //
 // ✏️ Change the base value below if your repo name is different.
 // ============================================================
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/nimrod-jirah-wedding/',
+  base: '/',   // ← change from '/nimrod-jirah-wedding/' to just '/'
 })
