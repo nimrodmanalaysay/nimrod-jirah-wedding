f3c55585a946b1059aeeba5e663a0fb9b13a2634
nimrodmanalaysay <manalaysaynimrod@gmail.com>
2026-06-29 15:47:29 +0800

    RSVP cancel changes 2

--- src/pages/rsvp/constants.js
+++ src/pages/rsvp/constants.js
@@ -1,5 +1,5 @@
 export const INVITEE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz4krEOAYvMChZ5m9Eak9bg5u7oInMJ15QuZTOlekmAtv6CEpk324tTwjY8tWPZRBgb/exec'
-export const RSVP_SCRIPT_URL    = 'https://script.google.com/macros/s/AKfycbxg7j66YG1qZ3BmhWAjinsC58QA3SlG7j1QMHfHV4KjJyNr-r8Ct_GcATvPSI0C8neV/exec'
+export const RSVP_SCRIPT_URL    = 'https://script.google.com/macros/s/AKfycbzDYMR7Nuc89wKkUt-5pqrv25U7eoXZXI8u-P4pm3OXB9luBcFlaLmIeL2QZ5WzXQdh/exec'
 
 export const INITIAL_FORM = {
   attendance: '',
